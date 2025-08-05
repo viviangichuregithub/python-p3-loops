a889eed4b75a2707016fe6f24a878d2177659de3
Vivian Gichure <viviangichure@gmail.com>
2025-08-05 19:24:53 +0300

    completed lab

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,26 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
     # code goes here!
-    pass
-
+    squared = []
+    for x in lst:
+        squared.append(x**2)
+    return squared
 def fizzbuzz():
     # code goes here!
-    pass
+   for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
